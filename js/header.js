b64083f86787d240a5c1ed374a9274214f9cf314
Olekka <noodarius0@gmail.com>
2023-07-29 15:59:33 +0300

    finish main adaptive

---
+++ js/header.js
@@ -0,0 +1,46 @@
+//menu burger
+
+const menuIcon = document.querySelector(".menu__icon");
+const menuBody = document.querySelector(".menu__body");
+if (menuIcon) {
+	menuIcon.addEventListener("click", function (e) {
+		document.body.classList.toggle("body--lock");
+		menuIcon.classList.toggle("menu__icon--active");
+		menuBody.classList.toggle("menu__body--active");
+		if (document.querySelector(".menu__item--opened")) {
+			document.querySelector(".menu__item--opened").classList.remove("menu__item--opened");
+		}
+	});
+}
+
+//scroll to section
+
+let menuLinks = document.querySelectorAll(".menu__link[data-goto]");
+if (menuLinks.length > 0) {
+	menuLinks.forEach((menuLink) => {
+		menuLink.addEventListener("click", onMenuLinkClick);
+	});
+	function onMenuLinkClick(e) {
+		const menuLink = e.target;
+		if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+			const gotoBlock = document.querySelector(menuLink.dataset.goto);
+			const gotoBlockValue =
+				gotoBlock.getBoundingClientRect().top + scrollY - document.querySelector(".header").offsetHeight;
+
+			if (menuIcon.classList.contains("menu__icon--active")) {
+				document.body.classList.remove("body--lock");
+				menuIcon.classList.remove("menu__icon--active");
+				menuBody.classList.remove("menu__body--active");
+				if (document.querySelector(".menu__item--opened")) {
+					document.querySelector(".menu__item--opened").classList.remove("menu__item--opened");
+				}
+			}
+
+			window.scrollTo({
+				top: gotoBlockValue,
+				behavior: "smooth",
+			});
+			e.preventDefault();
+		}
+	}
+}
